a312163229f512c8cb8530cc8ae836284da3549b
Edgar Lins <linss.edgar@gmail.com>
2023-03-22 21:58:27 -0300

    refactor

---
+++ js/index.js
@@ -0,0 +1,70 @@
+// default-import
+import Controls from "./controls.js"
+
+// named-import
+import Timer from "./timer.js"
+
+const buttonPlay = document.querySelector('.play')
+const buttonPause = document.querySelector('.pause')
+const buttonStop = document.querySelector('.stop')
+const buttonSet = document.querySelector('.set')
+const buttonSoundOn = document.querySelector('.sound-on')
+const buttonSoundOff = document.querySelector('.sound-off')
+const minutesDisplay = document.querySelector('.minutes')
+const secondsDisplay = document.querySelector('.seconds')
+let minutes = Number(minutesDisplay.textContent)
+let timerTimeOut
+
+const controls = Controls({
+    buttonPause,
+    buttonPlay,
+    buttonSet,
+    buttonStop
+})
+
+const timer = Timer({
+    minutesDisplay,
+    secondsDisplay,
+    timerTimeOut,
+    resetControls: controls.reset
+})
+
+//event-driven
+//programação imperativa
+//callback
+buttonPlay.addEventListener('click', function() {
+    controls.play()
+    timer.countDown()
+})
+
+buttonPause.addEventListener('click', function() {
+    controls.pause()
+    clearTimeout(timerTimeOut)
+})
+
+buttonStop.addEventListener('click', function() {
+    controls.reset()
+    timer.reset()
+})
+
+buttonSoundOff.addEventListener('click', function() {
+    buttonSoundOn.classList.remove('hide')
+    buttonSoundOff.classList.add('hide')
+})
+
+buttonSoundOn.addEventListener('click', function() {
+    buttonSoundOn.classList.add('hide')
+    buttonSoundOff.classList.remove('hide')
+})
+
+buttonSet.addEventListener('click', function() {
+    let newMinutes = controls.getMinutes()
+
+    if (!newMinutes) {
+        timer.reset()
+        return
+    }
+
+    minutes = newMinutes
+    timer.updateDisplay(minutes, 0)
+})
